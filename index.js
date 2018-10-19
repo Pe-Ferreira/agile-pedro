0d533085148a625e6dfdad22f21654e34cae8b25
Pedro Ferreira <pe.ferreira@outlook.com>
2018-10-19 02:31:41 -0300

    chart na tela

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var massaDeDados = gerarDadosDeTeste();
-var dadosDeVenda = 0;
 
 massaDeDados.categorias.forEach(function(item){
 	$('#categorias').append('<option value=' + item.id + '>' + item.nome + '</option>');
 });
 
 var idCategoriaSelecionada = $('#categorias option:selected').val();
-console.log(idCategoriaSelecionada);
-var categoria = buscaCategoriaPorId(parseInt(idCategoriaSelecionada), massaDeDados.categorias);
-dadosDeVenda = categoria.numeroDeVendas;
+var categoria = buscaObjetoPorId(parseInt(idCategoriaSelecionada), massaDeDados.categorias);
+var grafico = gerarGrafico(categoria.numeroDeVendas);
 
 $('body').on('change', '#categorias', function(){
 	var idCategoria = $(this).val();
@@ -18,6 +16,10 @@ $('body').on('change', '#categorias', function(){
 			$('#produtos').append('<option value=' + item.id + '>' + item.nome + '</option>');
 		}
 	});
+	var idCategoriaSelecionada = $('#categorias option:selected').val();
+	var categoria = buscaObjetoPorId(parseInt(idCategoriaSelecionada), massaDeDados.categorias);
+	grafico.data.datasets.data = [categoria.numeroDeVendas/2, categoria.numeroDeVendas/4, categoria.numeroDeVendas/8, categoria.numeroDeVendas/16];
+	grafico.update();
 	$('#marcas').empty();
 });
 
@@ -29,27 +31,39 @@ $('body').on('change', '#produtos', function(){
 			$('#marcas').append('<option value=' + item.id + '>' + item.nome + '</option>');
 		}
 	});
+	var idProdutoSelecionada = $('#produtos option:selected').val();
+	var produto = buscaObjetoPorId(parseInt(idProdutoSelecionada), massaDeDados.produtos);
+	grafico.data.datasets.data = [produto.numeroDeVendas/2, produto.numeroDeVendas/4, produto.numeroDeVendas/8, produto.numeroDeVendas/16];
+	grafico.update();
 });
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
+$('body').on('change', '#marcas', function(){
+	var idMarcaSelecionada = $('#marcas option:selected').val();
+	var marca = buscaObjetoPorId(parseInt(idMarcaSelecionada), massaDeDados.marcas);
+	grafico.data.datasets.data = [marca.numeroDeVendas/2, marca.numeroDeVendas/4, marca.numeroDeVendas/8, marca.numeroDeVendas/16];
+	grafico.update();
+});
+
+function gerarGrafico(dadosDeVenda){
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ["January", "February", "March", "April"],
         datasets: [{
             label: 'Vendas',
-            data: [dadosDeVenda, 19, 3, 5, 2, 3],
+            data: [dadosDeVenda/2, dadosDeVenda/4, dadosDeVenda/8, dadosDeVenda/16],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)'
             ],
             borderWidth: 1
         }]
@@ -63,33 +77,9 @@ var myChart = new Chart(ctx, {
             }]
         }
     }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	});
+	return myChart;
+}
 
 function gerarDadosDeTeste(){
 	var categoria1 = new Object();
@@ -111,14 +101,14 @@ function gerarDadosDeTeste(){
 	var produtos = [];
 	var marcas = [];
 
-	var QUANTIDADE_DE_PRODUTOS_POR_CATEGORIA = 4;
+	var QUANTIDADE_DE_PRODUTOS_POR_CATEGORIA = 3;
 	var QUANTIDADE_DE_MARCAS_POR_PRODUTO = 2;
 
 	for(i = 0; i < categorias.length; i++){
 		for(j = 0; j < QUANTIDADE_DE_PRODUTOS_POR_CATEGORIA; j++){
 			var produto = new Object();
 			produto.id = j + 1;
-			produto.nome = 'Produto' + (j + 1) + 'daCategoria' + categorias[i].nome;
+			produto.nome = 'Produto' + (j + 1) + 'daCat' + categorias[i].nome;
 			produto.numeroDeVendas = Math.floor(Math.random() * 300);
 			produto.categoria = categorias[i];
 			produtos.push(produto);
@@ -141,10 +131,10 @@ function gerarDadosDeTeste(){
 	return massaDeDados;
 }
 
-function buscaCategoriaPorId(id, categorias){
-	categorias.forEach(function(item){
-		if(item.id == id){
-			return item;
+function buscaObjetoPorId(id, arrayDeObjetos){
+	for(i = 0; i < arrayDeObjetos.length; i++){
+		if(arrayDeObjetos[i].id === id){
+			return arrayDeObjetos[i];
 		}
-	});
+	}
 }
